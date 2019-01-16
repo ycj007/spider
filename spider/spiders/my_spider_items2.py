b1adb77aefe78480b1846f99db666e798efd5499
chongjie.yuan <chongjie.yuan@mtime.com>
2019-01-16 17:04:42 +0800

    spider pages

--- spider/spiders/my_spider_items2.py
+++ spider/spiders/my_spider_items2.py
@@ -1,3 +1,5 @@
+from logging import info
+
 import scrapy
 
 
@@ -12,7 +14,13 @@ class itemSpider(scrapy.Spider):
             text = v.css('.text::text').extract_first()
             author = v.css('.author::text').extract_first()
             tags = v.css('.tags .tag::text').extract_first()
+            if not tags:
+                tags=[]
+            if not author:
+                    author = ""
             tags = ",".join(tags)
+
+
 
             filename="%s-语录.txt" % author
 
@@ -24,6 +32,11 @@ class itemSpider(scrapy.Spider):
                 f.write(100*"-")
                 f.write("\n")
                 f.close()
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        info(next_page)
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
 
 
